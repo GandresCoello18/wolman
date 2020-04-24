46e09f774cdb9e67b912137dfa17bc8757fd286c
GandresCoello18 <goyeselecoca@gmail.com>
2020-04-24 11:51:32 -0500

    path de social

--- network/router-api.js
+++ network/router-api.js
@@ -3,9 +3,9 @@ const twitter = require('./componentes/twitter');
 const facebook = require('./componentes/facebook');
 
 const router = function(server){
-    server.use('/api/auth', google);
-    server.use('/api_T/auth', twitter);
-    server.use('/api_F/auth', facebook);
+    server.use('/api/auth/google', google);
+    server.use('/api_T/auth/twitter', twitter);
+    server.use('/api_F/auth/facebook', facebook);
 }
 
 module.exports = router;
